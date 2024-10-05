76d73b762416df87de2f215b6787508152f38e9c
Idan Stark <idanstark42@gmail.com>
2024-10-06 00:19:13 +0300

    trying color coding

--- src/components/spyglass.jsx
+++ src/components/spyglass.jsx
@@ -51,7 +51,7 @@ export default function Spyglass () {
       Velocity ({velocities.length} samples):
       <ul style={{ maxHeight: '20vh', overflowY: 'auto' }}>
         {velocities.map((v, i) => (
-          <li key={i}>{v.join(', ')}</li>
+          <li key={i} style={{ color: (v[0] === 0 ? 'black' : (v[0] > 0 ? 'green' : 'red')) }}>{v.join(', ')}</li>
         ))}
       </ul>
       Position ({devicePositions.length} samples):
