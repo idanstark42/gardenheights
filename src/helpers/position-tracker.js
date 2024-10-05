cd6d063a6264acf86833f162e984985d3894dd6b
Idan Stark <idanstark42@gmail.com>
2024-10-06 01:47:33 +0300

    using orientation for the acceleration

--- src/helpers/position-tracker.js
+++ src/helpers/position-tracker.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react'
+import { mat4, quat } from 'gl-matrix'
 
 export function usePhonePosition () {
   const [orientation, setOrientation] = useState([0, 0, 0])
+  const [accelerationInDeviceRF, setAccelerationInDeviceRF] = useState([0, 0, 0])
   const [acceleration, setAcceleration] = useState([0, 0, 0])
   const [interval, setInterval] = useState(0)
   const [velocity, setVelocity] = useState([0, 0, 0])
@@ -9,23 +11,18 @@ export function usePhonePosition () {
 
   useEffect(() => {
     const handleOrientation = (event) => {
-      const alpha = event.alpha
-      const beta = event.beta
-      const gamma = event.gamma
+      const alpha = radians(event.alpha)
+      const beta = radians(event.beta)
+      const gamma = radians(event.gamma)
 
-      setOrientation([beta, alpha, -gamma])
+      setOrientation([beta, alpha, gamma])
     }
 
     const handleMotion = (event) => {
       const { acceleration, interval } = event
       if (!acceleration || (acceleration.x === 0 && acceleration.y === 0 && acceleration.z === 0)) return
       const { x, y, z } = acceleration
-      const newAcceleration = [
-        x * Math.cos(beta) * Math.cos(gamma) + y * Math.sin(gamma) - z * Math.sin(beta) * Math.cos(gamma),
-        x * Math.cos(beta) * Math.sin(gamma) - y * Math.cos(gamma) - z * Math.sin(beta) * Math.sin(gamma),
-        x * Math.sin(beta) + z * Math.cos(beta),
-      ]
-      setAcceleration(newAcceleration)
+      setAccelerationInDeviceRF([x, y, z])
       setInterval(interval / 1000)
     }
 
@@ -37,6 +34,18 @@ export function usePhonePosition () {
       window.removeEventListener('devicemotion', handleMotion)
     }
   }, [])
+
+  useEffect(() => {
+    const [x, y, z] = accelerationInDeviceRF
+    const [alpha, beta, gamma] = orientation
+    const rotationMatrix = getRotationMatrixFromEuler(alpha, beta, gamma)
+    const newAcceleration = [
+      x * rotationMatrix[0] + y * rotationMatrix[1] + z * rotationMatrix[2],
+      x * rotationMatrix[4] + y * rotationMatrix[5] + z * rotationMatrix[6],
+      x * rotationMatrix[8] + y * rotationMatrix[9] + z * rotationMatrix[10],
+    ]
+    setAcceleration(newAcceleration)
+  }, [accelerationInDeviceRF])
 
   useEffect(() => {
     const [ax, ay, az] = acceleration
@@ -51,4 +60,18 @@ export function usePhonePosition () {
   }, [velocity])
 
   return { orientation, acceleration, velocity, position }
+}
+
+function radians (degrees) {
+  return degrees * Math.PI / 180
+}
+
+function getRotationMatrixFromEuler(alpha, beta, gamma) {
+  const quaternion = quat.create()
+  quat.fromEuler(quaternion, alpha, beta, gamma)
+  
+  const rotationMatrix = mat4.create()
+  mat4.fromQuat(rotationMatrix, quaternion)
+
+  return rotationMatrix
 }
