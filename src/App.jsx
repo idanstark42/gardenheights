ac1b730fdcd8854e7c42a9b28e7678e122488498
Idan Stark <idanstark42@gmail.com>
2024-10-05 14:42:05 +0300

    using a dark map

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { useState, useEffect } from 'react'
 import { MapContainer, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import './App.css'
 
 
 export default function App() {
-  const [position, setPosition] = useState([51.505, -0.09])
+  const [position, setPosition] = useState([32.07327763966522, 34.80796244196957])
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
-      setPosition([position.coords.latitude, position.coords.longitude])
+      setPosition([32.07327763966522, 34.80796244196957])
     })
   }, [])
 
   return (
-    <MapContainer center={position}>
+    <MapContainer center={position} zoom={18} style={{ width: '100%', height: '100%' }}>
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png"
       />
     </MapContainer>
   )
