cd1a74cf48f0c1c0a84e6fb107b2ca437d734493
Idan Stark <idanstark42@gmail.com>
2024-10-05 17:17:09 +0300

    trying inline sessions

--- src/components/spyglass.jsx
+++ src/components/spyglass.jsx
@@ -1,38 +1,15 @@
-import { Canvas, createPortal, useThree } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { createXRStore, XR, XRDomOverlay, XROrigin } from '@react-three/xr'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import {} from '@react-three/drei'
 
 const store = createXRStore()
 
 export default function Spyglass() {
   const [bool, setBool] = useState(false)
-  const [text, setText] = useState('Hello World')
-
-  useEffect(() => {
-    store.onEnterAR = () => {
-      setText('Entered AR')
-    }
-    store.onExitAR = () => {
-      setText('Exited AR')
-    }
-  }, [])
-
-  const enter = async () => {
-    try {
-      await store.enterAR()
-    } catch (err) {
-      console.error(err)
-      setText('Error entering AR: ' + err.message)
-    }
-  }
-
-
   return (
     <>
-      <button onClick={enter}>Enter AR</button>
-      <button onClick={() => store.exitAR()}>Exit AR</button>
-      <p>{text}</p>
+      <button onClick={() => store.enterXR('inline')}>Enter AR</button>
       <Canvas style={{ width: '100%', flexGrow: 1 }}>
         <XR store={store}>
           <XROrigin />
