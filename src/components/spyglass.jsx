f518df8cd464abea5d032deb7b514bf28d027de7
Idan Stark <idanstark42@gmail.com>
2024-10-05 19:08:12 +0300

    showing history too

--- src/components/spyglass.jsx
+++ src/components/spyglass.jsx
@@ -48,13 +48,13 @@ export default function Spyglass () {
 
   return (
     <div style={{ position: 'absolute', bottom: 0, right: 0, padding: 10, width: '100%', height: '100%' }}>
-      Velocity:
+      Velocity ({velocities.length} samples):
       <ul style={{ maxHeight: '20vh', overflowY: 'auto' }}>
         {velocities.map((v, i) => (
           <li key={i}>{v.join(', ')}</li>
         ))}
       </ul>
-      Position:
+      Position ({devicePositions.length} samples):
       <ul style={{ maxHeight: '20vh', overflowY: 'auto' }}>
         {devicePositions.map((p, i) => (
           <li key={i}>{p.join(', ')}</li>
